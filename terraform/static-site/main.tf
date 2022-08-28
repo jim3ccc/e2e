18430a790ec4ea466551e7c30a5c684b76cf6a0e
meeej <ms0767467@gmail.com>
2022-08-27 21:16:30 -0700

    add terraform files

---
+++ terraform/static-site/main.tf
@@ -0,0 +1,24 @@
+terraform {
+    required_version = "0.12.20"
+}
+
+provider "aws" {
+    region = var.region
+    profile = var.profile
+}
+
+resource "aws_instance" "web" {
+    # Amazon Linux Application Machine Image (AMI)
+    ami = "ami-018d291ca9ffc002f"
+    # Free tier instance type
+    instance_type = "t2.micro"
+    # key pair created before
+    key_name = "dev1"
+    # default and devops_web security groups
+    vpc_security_group_ids = [ "sg-048fd3c3d88a86233", "sg-05bca4849397c856c" ]
+
+    tags = {
+        Name = var.name
+        group = var.group
+    }
+}
---
+++ terraform/static-site/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+    description = "default free tier region"
+}
+
+variable "profile" {
+    description = "default free tier region"
+}
+
+variable "name" {
+    description = "friendly name for the  ec2 instance"
+}
+
+variable "group" {
+    description = "name of the ansible dynamic inventory group"
+}
